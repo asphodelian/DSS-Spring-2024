025753679c72e549df1b84ad50c48cbc613ee58f
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-04-22 18:05:08 -0700

    converted categorical cols into factors for summary sake

--- Code Exploration/Obesity Explore.R
+++ Code Exploration/Obesity Explore.R
@@ -31,3 +31,22 @@ colnames(obese)[14] = "Screen Time"
 colnames(obese)[15] = "Snacking"
 colnames(obese)[16] = "Mode of Transport"
 colnames(obese)[17] = "Obesity Lvl"
+
+##############
+# Conversion #
+##############
+
+#obese$Gender <- replace(obese$Gender, obese$Gender == "Female", 0)
+#obese$Gender <- replace(obese$Gender, obese$Gender == "Male", 1)
+
+obese$Gender <- as.factor(obese$Gender)
+obese$`Alcohol Consumption` <- as.factor(obese$`Alcohol Consumption`)
+obese$`High Caloric Food Consumption` <- as.factor(obese$`High Caloric Food Consumption`)
+obese$`Calorie Count` <- as.factor(obese$`Calorie Count`)
+obese$SMOKE <- as.factor(obese$SMOKE)
+obese$family_history_with_overweight <- as.factor(obese$family_history_with_overweight)
+obese$Snacking <- as.factor(obese$Snacking)
+obese$`Mode of Transport` <- as.factor(obese$`Mode of Transport`)
+obese$`Obesity Lvl` <- as.factor(obese$`Obesity Lvl`)
+
+summary(obese)
