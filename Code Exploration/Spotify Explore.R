320a70b26cb660fe1e772123d7d170a733cfaa63
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-04-22 18:08:01 -0700

    Read in dataset, looked at names and summary stats

--- Code Exploration/Spotify Explore.R
+++ Code Exploration/Spotify Explore.R
@@ -6,3 +6,17 @@ library(readxl)
 library(caret)
 library(ggplot2)
 library(dplyr)
+
+###########
+# Read-In #
+###########
+
+spotify <- read.csv("C:/Users/knigh/OneDrive/Desktop/Github/DSS-Spring-2024/Possible Datasets/Spotify/playlist_2010to2023.csv")
+dim(spotify)
+
+#########
+# Names #
+#########
+
+names(spotify)
+summary(spotify)
