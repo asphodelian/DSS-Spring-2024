910e462129304211c67d793fe20dc5214c84ac1e
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-04-22 17:11:26 -0700

    Read dataset into code

--- Code Exploration/Student Explore.R
+++ Code Exploration/Student Explore.R
@@ -6,3 +6,5 @@ library(readxl)
 library(caret)
 library(ggplot2)
 library(dplyr)
+
+student <- read.csv("C:/Users/knigh/OneDrive/Desktop/Github/DSS-Spring-2024/Possible Datasets/Drop vs Success/dataset.csv")
