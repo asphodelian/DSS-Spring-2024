d38fbeb5f13806c8f4ca977a25282353610ba18d
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-05-04 14:26:32 -0700

    Made R script for plots, added libraries and dataset

---
+++ Code/Official Code/Obesity Plots.R
@@ -0,0 +1,19 @@
+#############
+# Libraries #
+#############
+
+library(readxl)
+library(caret)
+library(ggplot2)
+library(dplyr)
+
+###########
+# Dataset #
+###########
+
+obese <- read.csv("C:/Users/knigh/OneDrive/Desktop/Github/DSS-Spring-2024/Possible Datasets/Obesity/ObesityDataSet_raw_and_data_sinthetic.csv",
+                  stringsAsFactors = TRUE)
+
+#########
+# Plots #
+#########
