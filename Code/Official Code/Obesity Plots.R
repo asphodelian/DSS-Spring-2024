568bbf781df59b133fb7298059605e0aa31c5fa3
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-05-04 14:27:16 -0700

    Renamed columns

--- Code/Official Code/Obesity Plots.R
+++ Code/Official Code/Obesity Plots.R
@@ -15,5 +15,24 @@ obese <- read.csv("C:/Users/knigh/OneDrive/Desktop/Github/DSS-Spring-2024/Possib
                   stringsAsFactors = TRUE)
 
 #########
+# Names #
+#########
+
+names(obese)
+
+colnames(obese)[5] <- "Alcohol.Consumption"
+colnames(obese)[6] <- "High.Caloric.Food.Consumption"
+colnames(obese)[7] <- "Vegetable.Consumption"
+colnames(obese)[8] <- "Main.Meal.Consumption"
+colnames(obese)[9] <- "Calorie.Count"
+colnames(obese)[11] <- "Water.Consumption"
+colnames(obese)[12] <- "Family.History.Overweight"
+colnames(obese)[13] <- "Exercise.Activity"
+colnames(obese)[14] <- "Screen.Time"
+colnames(obese)[15] <- "Snacking"
+colnames(obese)[16] <- "Mode.of.Transport"
+colnames(obese)[17] <- "Obesity.Lvl"
+
+#########
 # Plots #
 #########
