05769eab6b8c3e5668a9d586327d1877d05c6dc1
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-05-05 01:30:57 -0700

    Added scatterplots/boxplots of vars against weight, because it would not accept obesity lvl

--- Code/Official Code/Obesity Plots.R
+++ Code/Official Code/Obesity Plots.R
@@ -83,5 +83,30 @@ legend("topright", labels, cex = 1.5, fill = colors)
 
 # scatterplot
 
-plot()
-
+plot(age, weight, main = "Weight vs Age", xlab = "Age", ylab = "Weight (kg)")
+plot(height, weight, main = "Weight vs Height", xlab = "Height (m)", 
+     ylab = "Weight (kg)")
+plot(alcohol, weight, main = "Weight vs Alcohol", xlab = "Alcohol Consumption", 
+     ylab = "Weight (kg)")
+plot(HCfood, weight, main = "Weight vs High Caloric Food", 
+     xlab = "High Caloric Food Consumption", ylab = "Weight (kg)")
+plot(veggie, weight, main = "Weight vs Vegetable", 
+     xlab = "Vegetable Consumption", ylab = "Weight (kg)")
+plot(main, weight, main = "Weight vs Main Meal", xlab = "Amount of Main Meals", 
+     ylab = "Weight (kg)")
+plot(count, weight, main = "Weight vs Calorie Count", 
+     xlab = "Do you calorie count?", ylab = "Weight (kg)")
+plot(smoke, weight, main = "Weight vs Smoking", xlab = "Do you smoke?", 
+     ylab = "Weight (kg)")
+plot(water, weight, main = "Weight vs Water", xlab = "Water Consumptiom", 
+     ylab = "Weight (kg)")
+plot(history, weight, main = "Weight vs Obesity History", 
+     xlab = "Do you have a family history of obesity?", ylab = "Weight (kg)")
+plot(exercise, weight, main = "Weight vs Exercise", 
+     xlab = "How often do you exercise?", ylab = "Weight (kg)")
+plot(screen, weight, main = "Weight vs Screen Time", 
+     xlab = "How long is your screen time?", ylab = "Weight (kg)")
+plot(snack, weight, main = "Weight vs Snacking", 
+     xlab = "Do you snack between meals?", ylab = "Weight (kg)")
+plot(transport, weight, main = "Weight vs Mode of Transport", 
+     xlab = "Main Mode of Transportation", ylab = "Weight (kg)")
