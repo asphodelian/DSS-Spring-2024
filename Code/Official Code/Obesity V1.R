7a5573c7d1cb2f8da615dc3e9e5dd16b6ff79b78
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-05-05 02:50:40 -0700

    New script to do official techniques & such; copy-pasted the libraries, read in, column names, and data split

---
+++ Code/Official Code/Obesity V1.R
@@ -0,0 +1,51 @@
+#############
+# Libraries #
+#############
+
+library(readxl)
+library(caret)
+library(ggplot2)
+library(dplyr)
+
+###########
+# Read-In #
+###########
+
+obese <- read.csv("C:/Users/knigh/OneDrive/Desktop/Github/DSS-Spring-2024/Possible Datasets/Obesity/ObesityDataSet_raw_and_data_sinthetic.csv",
+                  stringsAsFactors = TRUE)
+dim(obese)
+
+#########
+# Names #
+#########
+
+names(obese)
+
+colnames(obese)[5] <- "Alcohol.Consumption"
+colnames(obese)[6] <- "High.Caloric.Food.Consumption"
+colnames(obese)[7] <- "Vegetable.Consumption"
+colnames(obese)[8] <- "Main.Meal.Consumption"
+colnames(obese)[9] <- "Calorie.Count"
+colnames(obese)[11] <- "Water.Consumption"
+colnames(obese)[12] <- "Family.History.Overweight"
+colnames(obese)[13] <- "Exercise.Activity"
+colnames(obese)[14] <- "Screen.Time"
+colnames(obese)[15] <- "Snacking"
+colnames(obese)[16] <- "Mode.of.Transport"
+colnames(obese)[17] <- "Obesity.Lvl"
+
+###########
+# Summary #
+###########
+
+summary(obese)
+sum(is.na(obese)) # checking for missing values
+
+#######
+# Fit #
+#######
+
+train <- sample(1:nrow(obese),0.8*nrow(obese))
+test <- -train
+train.data <- obese[train,]
+test.data <- obese[-train,]
